a383e5004e8b6543e188f3383eed89b8bbcfe807
Rose <guta.rob@hotmail.nl>
2023-05-11 11:04:13 +0200

    added linksurveysquad + POST

---
+++ RockstarsAPI/Controllers/LinkSurveySquadController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RockstarsAPI.models;
+using System.Data.SqlClient;
+
+namespace RockstarsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinkSurveySquadController : ControllerBase
+    {
+        public readonly IConfiguration _Configuration;
+        public LinkSurveySquadController(IConfiguration Configuration)
+        {
+            _Configuration = Configuration;
+        }
+
+        [HttpPost]
+        public IActionResult LinkSurveySquad([FromBody] LinkSurveySquad LinkSurveySquad)
+        {
+            using (SqlConnection conn = new SqlConnection(_Configuration.GetConnectionString("SqlServer").ToString()))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO \"linksurveysquad\" (squadid, surveyid) VALUES (@squadid, @surveyid)", conn);
+                cmd.Parameters.AddWithValue("@squadid", LinkSurveySquad.Squadid);
+                cmd.Parameters.AddWithValue("@surveyid", LinkSurveySquad.Surveyid);
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected == 1)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return StatusCode(500);
+                }
+            }
+
+        }
+    }
+
+}
+
+
---
+++ RockstarsAPI/models/LinkSurveySquad.cs
@@ -0,0 +1,9 @@
+﻿namespace RockstarsAPI.models
+{
+    public class LinkSurveySquad
+    {
+        public int Squadid { get; set; }
+        public int Surveyid { get; set; }
+
+    }
+}
