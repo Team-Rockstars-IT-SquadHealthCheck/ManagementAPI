2b5abd8a99dfea5b5985877d0d0d77a9db0ed310
Bas Bakens <bas.bakens@live.nl>
2023-05-30 13:44:39 +0200

    added question api call

--- RockstarsAPI/Controllers/QuestionController.cs
+++ RockstarsAPI/Controllers/QuestionController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RockstarsAPI.models;
+using System.Data.SqlClient;
 
 namespace RockstarsAPI.Controllers
 {
@@ -7,5 +9,37 @@ namespace RockstarsAPI.Controllers
     [ApiController]
     public class QuestionController : ControllerBase
     {
+        public readonly IConfiguration _Configuration;
+        public QuestionController(IConfiguration Configuration)
+        {
+            _Configuration = Configuration;
+        }
+
+        [HttpPost]
+        [Route("/Question")]
+        public async Task<IActionResult> CreateQuestion(string question, string description, int surveyid, string desc_good, string desc_avg, string desc_bad)
+        {
+            using (SqlConnection conn = new SqlConnection(_Configuration.GetConnectionString("SqlServer").ToString()))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO question (question, description, surveyid, desc_good, desc_avg, desc_bad) VALUES (@question, @description, @surveyid, @desc_good, @desc_avg, @desc_bad)", conn);
+                cmd.Parameters.AddWithValue("@question", question);
+                cmd.Parameters.AddWithValue("@description", description);
+                cmd.Parameters.AddWithValue("@surveyid", surveyid);
+                cmd.Parameters.AddWithValue("@desc_good", desc_good);
+                cmd.Parameters.AddWithValue("@desc_avg", desc_avg);
+                cmd.Parameters.AddWithValue("@desc_bad", desc_bad);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected == 1)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return StatusCode(500);
+                }
+            }
+        }
     }
 }
---
+++ RockstarsAPI/models/Question.cs
@@ -0,0 +1,13 @@
+﻿namespace RockstarsAPI.models
+{
+    public class Question
+    {
+        public int id { get; set; }
+        public string question { get; set; }
+        public string description { get; set; }
+        public int surveyid { get; set; }
+        public string desc_good { get; set; }
+        public string desc_avg { get; set; }
+        public string desc_bad { get; set; }
+    }
+}
